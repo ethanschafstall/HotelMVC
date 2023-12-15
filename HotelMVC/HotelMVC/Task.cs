98c84a32001c488f61af0a0ae7e44f8e5bf39fa0
ethanschafstall <ethans9999@gmail.com>
2023-12-15 10:37:29 +0100

    Changed class variables

--- HotelMVC/HotelMVC/Task.cs
+++ HotelMVC/HotelMVC/Task.cs
@@ -8,14 +8,20 @@ namespace HotelMVC
 {
     public class Task
     {
-        private string _id;
+        private int _id;
         private string _name;
         private string _description;
         private string _status;
+        private DateTime _dateCreated;
+        private DateTime _dateStarted;
+        private DateTime _dateFinished;
 
-        public string Id { get => _id; set => _id = value; }
+        public int Id { get => _id; set => _id = value; }
         public string Name { get => _name; set => _name = value; }
         public string Description { get => _description; set => _description = value; }
         public string Status { get => _status; set => _status = value; }
+        public DateTime DateCreated { get => _dateCreated; set => _dateCreated = value; }
+        public DateTime DateStarted { get => _dateStarted; set => _dateStarted = value; }
+        public DateTime DateFinished { get => _dateFinished; set => _dateFinished = value; }
     }
 }
